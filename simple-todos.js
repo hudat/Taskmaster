26b5bb316a2b8e91ae86c8678072ccba462f9873
Husam Machlovi <hmachlovi@gmail.com>
2014-11-29 13:42:35 -0500

    created submit form, wired with task collection and set default sort to newest tasks first

--- simple-todos.js
+++ simple-todos.js
@@ -8,4 +8,29 @@ if (Meteor.isClient) {
       return Tasks.find({});
     }
   });
+  // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
+  Template.body.helpers({
+    tasks: function () {
+      // Show newest tasks first
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
 }
